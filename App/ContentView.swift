90e35444aebd550f8084ee4afad85e526cc4cac9
Miwa Tsubasa <k023c0026@g.neec.ac.jp>
2026-05-05 18:01:59 +0900

    setup

---
+++ App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kime
+//
+//  Created by Miwa Tsubasa on 2026/05/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ App/KimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KimeApp.swift
+//  Kime
+//
+//  Created by Miwa Tsubasa on 2026/05/05.
+//
+
+import SwiftUI
+
+@main
+struct KimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
